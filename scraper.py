75936a4fe62ec615957693ab64051d50d35186af
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2020-06-08 17:45:49 +0300

    Added regexp for scraping phone numbers

--- scraper.py
+++ scraper.py
@@ -7,35 +7,98 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    # #################### Step 1. Getting the URLs ####################
+    # #################### Step 1. Getting the URLs using Google search engine ####################
+    # file name to extract search strings from
     fileName = "search_phrases.txt"
+
     search_strs = getSearchStrings(fileName)
 
+    # extract base URLs and store only unique ones
+    urls = getBaseUrls( getURLs(search_strs) )
+
+    # for url in urls:
+    #     print(url)
+
+    url = urls[3]  # URL to test:
+    print(f"\ncurrent url to test: {url}")
+    html = getHTMLPageAsString(url)
+
+    # #################### Step 2. Parsing the URLs ####################
+
+    # use regular expression to extract emails from main page
+    emails = re.findall(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}', html)
+    # emails = re.findall('\w+@\w+\.{1}\w+', html)
+    # phones = re.findall(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})', html)
+    phones = re.findall("[(][\d]{3}[)][ ]?[\d]{3}-[\d]{4}", html)
+    # phone_number_regex_pattern = r"\(?\d{3}\)?[-.\s]\d{3}[-.\s]\d{4}"
+    unique_phones = set(phones)
+    unique_emails = set(emails)
+
+    print("EMAILS:")
+    for mail in unique_emails:
+        print(f"\t{mail}")
+
+    print("PHONES:")
+    for mail in unique_phones:
+        print(f"\t{phones}")
+
+    # extract all the <a> tags from page(string) html
+    soup = BeautifulSoup(html, "lxml")
+    links = []
+    tags = soup.find_all('a')
+    accepted_strings = {'Контакты', 'О компании'}
+
+    for tag in tags:
+        if (tag.text in accepted_strings):
+            links.append(tag.attrs['href'])
+
+    unique_links = set(links)
+
+    #  links of interest to follow in next commit
+    print("LINKS:")
+    for link in unique_links:
+        print(f"\t{link}")
+
+    # # ####################Step 3. Organizing extracted data ####################
+
+
+    # #################### FUNCTIONS ####################
+
+def getSearchStrings(fileName):
+    requests = []
+    if not os.path.isfile(fileName):
+        print("File path {fileName} does not exist. Exiting...")
+
+    with open(fileName, 'r') as f:
+        line = f.readline()
+        while line:
+            requests.append(line)
+            line = f.readline()
+    return requests
+
+
+def getBaseUrls(urls):
+    # Parse a URL into six components, returning a 6-item named tuple.
+    parsed = [urllib.parse.urlparse(url) for url in urls]
+    urls = list(set([x.scheme + '://' + x.hostname + '/' for x in parsed ]))
+    return urls
+
+def getURLs(search_strs):
     urls = []
-    for i in range(len(search_strs)):
-        for url in search(search_strs[i],  # The query you want to run
+    for str in search_strs:
+        for url in search(str,             # The query you want to run
                           tld='com',       # The top level domain
                           lang='ru',       # The language
                           num=10,          # Number of results per page
                           start=0,         # First result to retrieve
                           stop=10,         # Last result to retrieve
                           pause=2.0,       # Lapse between HTTP requests
-                          ):
+        ):
             urls.append(url)
-    # #################### Step 2. Parsing the URLs ####################
-    # Get rid of URL duplicates (extract base URLs and store only unique ones)
-    base_urls = getBaseUrls(urls)
-    urls = list(set(base_urls))
+    return urls
 
-    # print unique_urls
-    # print("unique_urls: ")
-    # for url in urls:
-    #     print(url)
 
-    # URL to test:
-    url = urls[7]
-    print(f"\ncurrent url to test: {url}")
-
+def getHTMLPageAsString(url):
     # create headers for http-requests using fake user-agent
     # simple prevention measure against being blocked - to fake a browser visit
     ua = UserAgent()
@@ -46,8 +109,8 @@ def main():
         )
     # print(f"type(request) = {type(request)}")
 
+    # urllib.request.urlopen(...) returns object of type: http.client.HTTPResponse
     try:
-        # urllib.request.urlopen(...) returns object of type: http.client.HTTPResponse
         with urllib.request.urlopen(request, timeout=10) as response:
             raw_data = response.read()
     except timeout:
@@ -70,56 +133,8 @@ def main():
 
     # convert bytes to string using the proper charset
     html = raw_data.decode(charset)
-    # print(f"type(html) = {type(html)}")
-    # print(f"len(html) = {len(html)}")
 
-    # use regular expression to extract emails from main page
-    emails = re.findall(r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}', html)
-    unique_emails = set(emails)
-    # emails = re.findall('\w+@\w+\.{1}\w+', html)
-    print("EMAILS:")
-    for mail in unique_emails:
-        print(f"\t{mail}")
-
-    # extract all the <a> tags from page(string) html
-    soup = BeautifulSoup(html, "lxml")
-    links = []
-    tags = soup.find_all('a')
-    accepted_strings = {'Контакты', 'О компании'}
-    for tag in tags:
-        if (tag.text in accepted_strings):
-            links.append(tag.attrs['href'])
-    unique_links = set(links)
-
-    #  links of interest to follow in next commit
-    print("LINKS:")
-    for link in unique_links:
-        print(f"\t{link}")
-
-    # # ####################Step 3. Organizing extracted data ####################
-
-
-    # #################### FUNCTIONS ####################
-
-def getSearchStrings(fileName):
-    requests = []
-    # filepath = sys.argv[1]
-    # filePath = os.path.relpath(fileName)
-
-    if not os.path.isfile(fileName):
-        print("File path {fileName} does not exist. Exiting...")
-
-    with open(fileName, 'r') as f:
-        line = f.readline()
-        while line:
-            requests.append(line)
-            line = f.readline()
-    return requests
-
-
-def getBaseUrls(urls):
-    parsed = [urllib.parse.urlparse(url) for url in urls]
-    return [x.scheme + '://' + x.hostname + '/' for x in parsed ]
+    return html
 
 
 if __name__ == '__main__':
